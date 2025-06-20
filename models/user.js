adce1b78ef0e384ca72b2d2d5310c0700b70932f
Satyam Raj <satyambpl.2000@gmail.com>
2025-06-20 15:22:23 +0530

    Update user.js

--- models/user.js
+++ models/user.js
@@ -38,7 +38,6 @@ const userSchema=mongoose.Schema({
         type:mongoose.Schema.Types.ObjectId,
         ref:'vender'
     }],
-    orders:Number,
 })
 
 module.exports=mongoose.model('User',userSchema,'user')//---->model name, schema name, collection name;
