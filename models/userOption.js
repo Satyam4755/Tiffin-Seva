6059a74c83988d794d0bb3d3d32620a5fc67403c
Satyam Raj <satyambpl.2000@gmail.com>
2025-09-01 19:59:00 +0530

    Update userOption.js

--- models/userOption.js
+++ models/userOption.js
@@ -4,7 +4,7 @@ const guestOptionSchema = mongoose.Schema({
   guest: { type: mongoose.Schema.Types.ObjectId, ref: 'User', required: true },
   mealSelected: { type: String, required: true },
   Location: { type: String },
-  vendor: { type: mongoose.Schema.Types.ObjectId, ref: 'vender', required: true },
+  vendor: { type: mongoose.Schema.Types.ObjectId, ref: 'User', required: true },
   createdAtU20: { type: Date, default: Date.now, expires: 60*60*6 }  // TTL set to 6 hours
 });
 
