39e637a8ba8bbf887f5549d2de4588c2e747e67b
Satyam Raj <satyambpl.2000@gmail.com>
2025-09-21 18:45:21 +0530

    Update policyRoutes.js

--- routes/policyRoutes.js
+++ routes/policyRoutes.js
@@ -4,27 +4,27 @@ const router = express.Router();
 
 // Privacy Policy
 router.get('/privacy-policy', (req, res) => {
-  res.render('privacy-policy');
+  res.render('privacy');
 });
 
 // Terms & Conditions
 router.get('/terms-and-conditions', (req, res) => {
-  res.render('terms-and-conditions');
+  res.render('terms');
 });
 
 // Cancellation & Refund
 router.get('/cancellation-refund-policy', (req, res) => {
-  res.render('cancellation-refund-policy');
+  res.render('refund');
 });
 
 // Shipping & Delivery
 router.get('/shipping-delivery-policy', (req, res) => {
-  res.render('shipping-delivery-policy');
+  res.render('shipping');
 });
 
 // Contact Us
 router.get('/contact-us', (req, res) => {
-  res.render('contact-us');
+  res.render('contact');
 });
 
 module.exports = router;
